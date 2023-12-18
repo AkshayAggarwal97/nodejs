be9595a38319b360ffb31f09cb3eeb410354410b
AkshayAggarwal97 <akshay.technogetic@gmail.com>
2023-12-18 16:40:33 +0530

    added new route code

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ import userRouter from "./src/routes/userRoute.js";
 app.use(express.json());
 app.use("/api/v1", userRouter);
 
+app.get('/', function(req, res) {
+  res.send('Welcome')
+})
+
 const port = 5000;
 app.listen(port, () => {
   console.log(`Server is running on ${port}... `);
